288a4dd7e26be2ca1451770a427ff36edcc6e61e
David Faulkenberry <david.faulkenberry@gmail.com>
2015-09-10 20:49:30 -0400

    Some format constraints on phone and SPIRE
    For Twilio and Shibboleth purposes, respectively

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
             presence: true
   validates :spire, :email, :phone,
             uniqueness: true
+  validates :spire,
+            format: { with: /\d{8}@umass.edu/,
+                      message: 'must be 8 digits followed by @umass.edu' }
+  validates :phone,
+            format: { with: /\+1\d{10}/,
+                      message: 'must be 1+ followed by 10 digits' }
 
   def full_name
     "#{first_name} #{last_name}"
