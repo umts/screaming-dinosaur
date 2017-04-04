43acf343563abd35d83f961031d45655c8024aa9
David Faulkenberry <david.faulkenberry@gmail.com>
2017-04-04 11:28:23 -0400

    No special user validations anymore

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 describe User do
-  describe 'validations' do
-    it 'allows only one fallback user' do
-      expect { create :user, is_fallback: true }
-        .not_to raise_error
-      expect { create :user, is_fallback: true }
-        .to raise_error(ActiveRecord::RecordInvalid,
-                        'Validation failed: ' \
-                        'Fallback may be true for only one user')
-    end
-  end
   describe 'full_name' do
     it 'returns first name followed by last name' do
       user = create :user
