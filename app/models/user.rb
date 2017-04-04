2799f8d89031c66b243d03130eb3a99fd62dea91
David Faulkenberry <david.faulkenberry@gmail.com>
2017-04-04 14:42:30 -0400

    Validate for rotation presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :assignments, dependent: :restrict_with_error
   has_and_belongs_to_many :rotations
 
-  validates :first_name, :last_name, :spire, :email, :phone,
+  validates :first_name, :last_name, :spire, :email, :phone, :rotations,
             presence: true
   validates :spire, :email, :phone,
             uniqueness: true
