1226cf33517395d6e14b55578c3307b4ba3bd304
David Faulkenberry <david.faulkenberry@gmail.com>
2017-04-04 11:30:57 -0400

    Move specs from Assignment to Rotation
    Since that's how we moved the method in question as well

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -28,51 +28,6 @@ RSpec.describe Assignment do
     end
   end
 
-  describe 'generate_rotation' do
-    before :each do
-      @user_1 = create :user
-      @user_2 = create :user
-      @user_3 = create :user
-      start_date = Date.today
-      # A day short of four weeks, to test that the end date
-      # is a day short as well
-      end_date = (4.weeks.since - 2.days).to_date
-      starting_user_id = @user_2.id
-      @assignments = Assignment.generate_rotation(
-        [@user_1.id, @user_2.id, @user_3.id],
-        start_date,
-        end_date,
-        starting_user_id
-      )
-      expect(@assignments.size).to be 4
-    end
-    it 'creates the expected assignments (part 1)' do
-      assignment = @assignments[0]
-      expect(assignment.user).to eql @user_2 # starts in the correct place
-      expect(assignment.start_date).to eql Date.today
-      expect(assignment.end_date).to eql 6.days.since.to_date
-    end
-    it 'creates the expected assignments (part 2)' do
-      assignment = @assignments[1]
-      expect(assignment.user).to eql @user_3
-      expect(assignment.start_date).to eql 1.week.since.to_date
-      expect(assignment.end_date).to eql 13.days.since.to_date
-    end
-    it 'creates the expected assignments (part 3)' do
-      assignment = @assignments[2]
-      expect(assignment.user).to eql @user_1 # wraps back around
-      expect(assignment.start_date).to eql 2.weeks.since.to_date
-      expect(assignment.end_date).to eql 20.days.since.to_date
-    end
-    # this one is significant because there are more weeks than
-    # people - just make sure the modular arithmetic works
-    it 'creates the expected assignments (part 4)' do
-      assignment = @assignments[3]
-      expect(assignment.user).to eql @user_2
-      expect(assignment.start_date).to eql 3.weeks.since.to_date
-      expect(assignment.end_date).to eql 26.days.since.to_date
-    end
-  end
 
   describe 'next_rotation_start_date' do
     before :each do
