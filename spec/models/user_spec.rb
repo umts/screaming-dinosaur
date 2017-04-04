4250a08cf126d7cefabf6ba82f4ed07744a215ad
David Faulkenberry <david.faulkenberry@gmail.com>
2017-04-04 11:28:42 -0400

    Remove tests for disused model method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,13 +14,4 @@ describe User do
         .to eql [user.last_name, user.first_name].join(', ')
     end
   end
-  describe 'self.fallback' do
-    it 'returns the fallback user if one is present' do
-      fallback = create :user, is_fallback: true
-      expect(User.fallback).to eql fallback
-    end
-    it 'returns nil if no fallback user is present' do
-      expect(User.fallback).to be nil
-    end
-  end
 end
