ae1bc1f6ad2e7d4b588bba920dc8c98ad2b3429d
David Faulkenberry <david.faulkenberry@gmail.com>
2017-04-18 14:54:29 -0400

    Fix overlapping assignments issue
    Ref #54

--- app/mailers/assignments_mailer.rb
+++ app/mailers/assignments_mailer.rb
@@ -6,7 +6,8 @@ class AssignmentsMailer < ActionMailer::Base
     @roster = assignment.roster
     @user = user
     @start_date = assignment.start_date.strftime '%A, %B %e'
-    @end_date = assignment.end_date.strftime '%A, %B %e'
+    # Assignments effectively end at the switchover hour on the following day.
+    @end_date = assignment.end_date.succ.strftime '%A, %B %e'
     @time = HOURS.fetch CONFIG[:switchover_hour]
     mail to: user.email,
          subject: "Upcoming on-call (#{@roster.name})"
