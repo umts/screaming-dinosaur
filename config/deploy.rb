db9da303b374f68b874d1ac11effb116f3327808
anbranin <keichelman@gmail.com>
2017-06-13 10:34:13 -0400

    whenever command needs to use sudo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.5.0'
 
-set :application, :screaming_dinosaur
+set :application, 'screaming_dinosaur'
 
 set :scm, :git
 set :repo_url, 'git@github.com:umts/screaming-dinosaur.git'
@@ -12,6 +12,8 @@ set :keep_releases, 5
 set :deploy_to, "/srv/#{fetch :application}"
 
 set :log_level, :info
+
+set :whenever_command, [:sudo, :bundle, :exec, :whenever]
 
 set :linked_files, fetch(:linked_files, []).push(
   'config/database.yml',
