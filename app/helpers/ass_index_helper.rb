a9680cc43843b50ebf07b94a658c7c252602b350
Justin <juforgue@gmail.com>
2017-07-12 14:08:42 -0400

    Drastic restyle of calendar
    - Created new scss file for calendar and removed calendar css from index.scss
    - Created view helper for assignment index
    - Rearranged assignment index page
    - Calendar highlights current day, current user's assignments, and
      indicates assignment change

---
+++ app/helpers/ass_index_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module AssIndexHelper
+
+  def cal_day(assignment:, day:)
+    whatDay = ''
+    if assignment.present?
+      if day == assignment.start_date
+        whatDay = 'ass-start'
+      elsif day == assignment.end_date
+        whatDay = 'ass-end'
+      end
+      if assignment.user == @current_user
+        whatDay += ' ass-week'
+      end
+    end
+    if day == Date.current
+      whatDay = 'current-day'
+    end
+    whatDay
+  end
+end
