4f8df11a737475a16dce74037c264475b7dd7462
Justin <juforgue@gmail.com>
2018-03-23 14:08:47 -0400

    Complete feature tests and create system tests

---
+++ spec/system/users/index_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'user pages' do
+  let(:admin_membership) { create :membership, admin: true }
+  let(:admin) { admin_membership.user }
+  context 'deactivating a user' do
+    before :each do
+      set_current_user(admin)
+      visit root_path
+      click_link 'Manage users'
+      click_button 'Deactivate'
+      @dialog = page.driver.browser.switch_to.alert
+    end
+    it 'warns current user before deactivation with a pop up' do
+      expect(@dialog.text).to eq 'Deactivating user will delete all upcoming assignments.'
+    end
+    it 'deactivates a user' do
+      @dialog.accept
+      expect(page).to have_selector 'div', text: 'User has been updated.'
+      expect(page).not_to have_selector 'td', text: admin.first_name
+    end
+  end
+end
