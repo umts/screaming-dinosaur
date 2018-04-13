d4d02934d59b8d03a2531664bc7c5bf38987816b
Justin <juforgue@gmail.com>
2018-04-13 14:20:40 -0400

    Convert global JS to JQuery

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-    var copyTextBtn = document.querySelector('.copy-text-btn');
+    var copyTextBtn = $('.copy-text-btn');
 
-    copyTextBtn.addEventListener('click', function (event) {
-        var copyTextarea = document.querySelector('.copy-text');
+    $(copyTextBtn).on('click', function() {
+        var copyTextarea = $('.copy-text');
         copyTextarea.focus();
         copyTextarea.select();
-
         try {
             var successful = document.execCommand('copy');
             var msg = successful ? 'successful' : 'unsuccessful';
