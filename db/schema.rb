ddf7a60b1dd161dd5ddfff5dbdb64a4a1819ac7a
Justin <juforgue@gmail.com>
2019-06-04 15:08:02 -0400

    Parameterize url

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330224537) do
+ActiveRecord::Schema.define(version: 2018_03_30_224537) do
 
-  create_table "assignments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "assignments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.date "start_date"
     t.date "end_date"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180330224537) do
     t.integer "roster_id"
   end
 
-  create_table "memberships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "memberships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "roster_id"
     t.integer "user_id"
     t.boolean "admin", default: false
@@ -29,14 +29,14 @@ ActiveRecord::Schema.define(version: 20180330224537) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "rosters", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "rosters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "fallback_user_id"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "spire"
@@ -46,11 +46,11 @@ ActiveRecord::Schema.define(version: 20180330224537) do
     t.datetime "updated_at", null: false
     t.boolean "reminders_enabled", default: true
     t.boolean "change_notifications_enabled", default: true
-    t.string "calendar_access_token"
     t.boolean "active", default: true
+    t.string "calendar_access_token"
   end
 
-  create_table "versions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "versions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "item_type", limit: 191, null: false
     t.integer "item_id", null: false
     t.string "event", null: false
