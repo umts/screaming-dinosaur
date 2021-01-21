84a41521d17e8886979feb7f7965561ee1b96c5d
Matt Moretti <werebus@gmail.com>
2021-01-21 14:26:45 -0500

    Main Layout and Nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def active_class(link_path)
-    'active' if current_page?(link_path)
+  def nav_link_item(text, path)
+    classes = %w[nav-item mx-2]
+    classes << 'active' if current_page?(path)
+    content_tag 'li', class: classes do
+       link_to text, path, class: 'nav-link'
+    end
   end
 end
