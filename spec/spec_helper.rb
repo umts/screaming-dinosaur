5365811928597d84d0e233c1c29eaaa29bf0609f
Matt Moretti <werebus@gmail.com>
2021-01-23 21:41:02 -0500

    Silence puma server startup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ require 'rack_session_access/capybara'
 
 ActiveRecord::Migration.maintain_test_schema!
 Timecop.safe_mode = true
+Capybara.server = :puma, { Silent: true }
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
