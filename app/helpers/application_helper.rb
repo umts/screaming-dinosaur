631bc08f83e6643acf4e7a07672ceaee3bded0dc
Matt Moretti <werebus@gmail.com>
2021-02-17 15:33:17 -0500

    Remove pretty-much needless "constants"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def active_class(link_path)
     'active' if current_page?(link_path)
   end
+
+  def switchover_time(hour)
+    Time.new.change(hour: hours).strftime('%I:%M %p')
+  end
 end
