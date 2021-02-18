6bc9d08cac14a0b43c8b4661f1a25f30bb167612
Matt Moretti <werebus@gmail.com>
2021-02-18 16:11:07 -0500

    Less complicated spire and phone sequences

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
   factory :user do
     sequence(:first_name) { |n| "First#{n}" }
     sequence(:last_name) { |n| "Last#{n}" }
-    sequence(:spire) { |n| n.to_s.rjust(8, '0') + '@umass.edu' }
+    sequence(:spire) { |n| format('%08d@umass.edu', n) }
     sequence(:email) { |n| "user#{n}@umass.edu" }
-    sequence(:phone) { |n| '+1' + n.to_s.rjust(10, '0') }
+    sequence(:phone) { |n| format('+1%010d', n) }
     rosters { [create(:roster)] }
   end
 end
