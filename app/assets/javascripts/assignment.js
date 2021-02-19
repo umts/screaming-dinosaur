29b8a80329d6b97bc39fee342a7f078359acf94d
Matt Moretti <werebus@gmail.com>
2021-02-19 12:36:30 -0500

    Styled feed form; fixed tooltips.

--- app/assets/javascripts/assignment.js
+++ app/assets/javascripts/assignment.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-    // enable bootstrap tooltips
-    $("[data-toggle='tooltip']").tooltip('enable');
-    var copyTextBtn = $('.copy-text-btn');
-    $(copyTextBtn).on('click', function() {
-        var copyTextarea = $('.copy-text');
-        copyTextarea.focus();
-        copyTextarea.select();
-        try {
-            var successful = document.execCommand('copy');
-            console.log('Copied successfully');
-        } catch (err) {
-            console.log('Unable to copy');
-        }
-        if(successful) {
-            $('.copy-tooltip').attr('title', 'Copied successfully!')
-                .tooltip('fixTitle')
-                .tooltip('show');
-            console.log('Tooltip title updated')
-        }
-    });
+  // enable bootstrap tooltips
+  $("[data-toggle='tooltip']").tooltip('enable');
+
+  $('.copy-text-btn').on('click', function() {
+    var copyTextarea = $('.copy-text');
+    copyTextarea.focus();
+    copyTextarea.select();
+    try {
+      var successful = document.execCommand('copy');
+    } catch (err) {
+      console.log('Unable to copy');
+    }
+    if(successful) {
+      var title = 'Copied successfully!';
+      $('.copy-tooltip').attr('title', title)
+        .tooltip('dispose')
+        .tooltip({'title': title})
+        .tooltip('show');
+    }
+  });
 });
