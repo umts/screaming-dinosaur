029f9d613977d3c90437b2746f6124c0c67e3aa8
Matt Moretti <werebus@gmail.com>
2021-02-22 15:50:20 -0500

    Make the calendar feed form a bit more accessible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def calendar_instructions
+    'Use this address to subscribe to this calendar in ' \
+    'another application (i.e. Google Calendar).'
+  end
+
   def nav_link_item(text, path)
     classes = %w[nav-item mx-2]
     classes << 'active' if current_page?(path)
