8360a6ba380a584610ba75a92115f3a9eca1b192
Matt Moretti <werebus@gmail.com>
2021-02-22 16:30:38 -0500

    tag is a bit different than content_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   def nav_link_item(text, path)
     classes = %w[nav-item mx-2]
     classes << 'active' if current_page?(path)
-    tag 'li', class: classes do
+    tag.li class: classes do
       link_to text, path, class: 'nav-link'
     end
   end
