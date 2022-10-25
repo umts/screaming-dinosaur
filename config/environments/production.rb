752bfb95e64a457281bd73e88822b575b1a0615e
Matt Moretti <werebus@gmail.com>
2022-10-25 15:08:55 -0400

    Enable the exception-notification middeware
    Closes #200

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.middleware.use ExceptionNotification::Rack,
+    email: { email_prefix: 'umts/screaming-dinosaur exception: ',
+             sender_address: 'Screaming Dinosaur <transit-it@admin.umass.edu>',
+             exception_recipients: %w[programmers@admin.umass.edu] }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
