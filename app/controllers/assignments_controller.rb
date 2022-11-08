f072107f0a9887c111e4134aab781e02f7fbdaff
jforgue <26842354+bandogora@users.noreply.github.com>
2022-11-08 16:03:29 -0500

    Don't call each on flash[:errors]
    - Display flash errors in the standard fashion
    - Update require_taking_ownership error to be a regular string
    - Test generate rotation starting user not in rotation

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -143,11 +143,9 @@ class AssignmentsController < ApplicationController
   def require_taking_ownership
     return true if @current_user.admin_in?(@roster) || taking_ownership?
 
-    flash[:errors] = [<<-TEXT]
-      You may only edit or create assignments such that you become on call.
-      The intended new owner of this assignment must take it themselves.
-      Or, a roster administrator can perform this change for you.
-    TEXT
+    flash[:errors] = 'You may only edit or create assignments such that you become on call. ' \
+      'The intended new owner of this assignment must take it themselves. ' \
+      'Or, a roster administrator can perform this change for you.'
     redirect_back fallback_location: roster_assignments_path(@roster)
     false
   end
