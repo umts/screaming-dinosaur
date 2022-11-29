6df60d4935fbb53b1b1b300b3613500a459c9c55
jforgue <26842354+bandogora@users.noreply.github.com>
2022-11-29 13:35:28 -0500

    Update tooltip enable JS

--- app/assets/javascripts/assignment.js
+++ app/assets/javascripts/assignment.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   // Enable bootstrap tooltips
-  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  tooltipTriggerList.forEach(function(tooltipTriggerNode) {
+    new bootstrap.Tooltip(tooltipTriggerNode);
+  });
 
   $('.copy-tooltip').on('click', function() {
     var copyTextarea = $('.copy-text');
