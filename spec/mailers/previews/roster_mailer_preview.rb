8b39066716b7a37199de6f562d241bba83aa46d0
jforgue <26842354+bandogora@users.noreply.github.com>
2023-01-31 13:19:26 -0500

    Update roster mailer preview

--- spec/mailers/previews/roster_mailer_preview.rb
+++ spec/mailers/previews/roster_mailer_preview.rb
@@ -2,6 +2,8 @@
 
 class RosterMailerPreview < ActionMailer::Preview
   def open_dates_alert
-    RosterMailer.with(roster: Roster.first).open_dates_alert
+    roster = Roster.first
+    open_dates = roster.uncovered_dates_between(Time.zone.now, 2.weeks.from_now)
+    RosterMailer.with(roster: roster, open_dates: open_dates).open_dates_alert
   end
 end
