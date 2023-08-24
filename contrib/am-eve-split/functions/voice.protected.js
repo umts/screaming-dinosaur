c1a21f175ddadbfa0b69edbdb5f61224770a5410
Matt Moretti <werebus@gmail.com>
2023-08-24 13:29:15 -0400

    Initial project creation and barely working scripts

---
+++ contrib/am-eve-split/functions/voice.protected.js
@@ -0,0 +1,24 @@
+exports.handler = (context, event, callback) => {
+  let twiml = new Twilio.twiml.VoiceResponse();
+
+  const now = new Date();
+  const formatOptions = {
+    hour: 'numeric',
+    hour12: false,
+    weekday: 'long',
+    timeZone: 'America/New_York',
+  };
+  const formatter = new Intl.DateTimeFormat('en-US', formatOptions);
+
+  const formattedDate = formatter.format(now).split(', ');
+  const day = formattedDate[0];
+  const hour = Number(formattedDate[1]);
+  const isWeekend = ['Sunday', 'Saturday'].includes(day);
+
+  if (hour >= context.switchover_hour || hour < context.day_start) {
+    twiml.redirect({method: 'GET'}, context.eve_roster_voice_url);
+  } else {
+    twiml.redirect({method: 'GET'}, context.day_roster_voice_url);
+  }
+  return callback(null, twiml);
+}
