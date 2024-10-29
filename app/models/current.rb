c36637260839348974c5c0e6c76d6a9445221874
Matt Moretti <werebus@gmail.com>
2024-10-29 15:25:27 -0400

    Use Current.user to set/unset Paper Trail whodunnit

--- app/models/current.rb
+++ app/models/current.rb
@@ -2,4 +2,11 @@
 
 class Current < ActiveSupport::CurrentAttributes
   attribute :user
+
+  resets { PaperTrail.request.whodunnit if PaperTrail.request.enabled? }
+
+  def user=(user)
+    PaperTrail.request.whodunnit = user&.id if PaperTrail.request.enabled?
+    super
+  end
 end
