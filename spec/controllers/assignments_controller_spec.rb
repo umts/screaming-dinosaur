728c92642ff6a41356cba9f41551dacb04615cdf
Alex Locke <alocke_8181@yahoo.com>
2025-01-02 10:34:51 -0500

    spec changes per benmelz

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -124,7 +124,7 @@ RSpec.describe AssignmentsController do
     let :params do
       { roster_id: roster.id,
         start_date: Time.zone.today.to_fs(:db),
-        end_date: Date.tomorrow.to_fs(:db),
+        end_date:,
         user_ids:,
         starting_user_id: }
     end
@@ -132,6 +132,7 @@ RSpec.describe AssignmentsController do
     let(:user_ids) { Array.new(3) { roster_user(roster).id.to_s } }
     let(:starting_user_id) { user_ids[1] }
     let(:assignment) { create :assignment }
+    let(:end_date) { Date.tomorrow.to_fs(:db) }
 
     before do
       when_current_user_is :whoever
@@ -171,12 +172,7 @@ RSpec.describe AssignmentsController do
       end
 
       context 'when the end date is before the start date' do
-        before { params[:end_date] = Date.yesterday.to_fs(:db) }
-
-        it 'warns the end date is before the start date' do
-          submit
-          expect(flash[:errors]).not_to be_empty
-        end
+        let(:end_date) { Date.yesterday.to_fs(:db) }
 
         it 'redirects back' do
           expect { submit }.to redirect_back
