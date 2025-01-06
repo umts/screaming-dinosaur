4ead8f73223d9515eba781b4d603b6c9afde46b8
Alex Locke <alocke_8181@yahoo.com>
2025-01-06 14:11:00 -0500

    first draft of rotation generator model

--- app/models/assignment/rotation_generator.rb
+++ app/models/assignment/rotation_generator.rb
@@ -6,13 +6,43 @@ class Assignment < ApplicationRecord
     include ActiveModel::Attributes
 
     attribute :roster_id, :integer
-    attribute :user_id, :integer
+    attribute :user_ids
+    attribute :start_user_id, :integer
     attribute :start_date, :date
     attribute :end_date, :date
 
     validates :roster, presence: true
-    validates :user, presence: true
+    validates :user_ids, presence: true
+    validates :start_user_id, presence: true, inclusion: { in: :user_ids }
     validates :start_date, presence: true
     validates :end_date, presence, comparison: { greater_than_or_equal_to: :start_date }
+
+    def roster
+      @roster ||= Roster.find_by(id: roster_id)
+    end
+
+    def generate
+      generate!
+      true
+    rescue ActiveModel::ValidationError, ActiveRecord::RecordInvalid
+      false
+    end
+
+    def generate!
+      validate!
+      ActiveRecord::Base.transaction do
+        send_notifications @roster.generate_assignments(user_ids, start_date,
+                                                        end_date, start_user_id)
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      errors.merge! e.record.errors
+      raise e
+    end
+
+    def send_notifications(assignments)
+      assignments.each do |assignment|
+        assignment.notify :owner, of: :new_assignment, by: Current.user
+      end
+    end
   end
 end
