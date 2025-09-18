e80dbebffa2f314616960a96ab91ec009ddce575
benmelz <ben@melz.me>
2025-09-18 12:14:54 -0400

    reconfigure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// TODO: Move to eslint.config.js when on eslint 9+.
+module.exports = {
+  ignorePatterns: [
+    'coverage/*',
+    'node_modules/*',
+    'public/assets/*',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true,
+  },
+  globals: {
+    'bootstrap': 'readonly',
+    'FullCalendar': 'readonly',
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+  ],
+  rules: {
+    'max-len': ['error', {code: 120}],
+  },
+  overrides: [{files: ['*.config.js', '.*rc.js'], env: {node: true}}],
+};
