fb5b08c6834931b10a8ed5dc6f76f882d8320243
benmelz <ben@melz.me>
2025-09-18 12:50:27 -0400

    rewrite copy button

--- app/assets/javascripts/assignment.js
+++ app/assets/javascripts/assignment.js
@@ -1,20 +1,22 @@
-$(function() {
-  $('.copy-tooltip').on('click', function() {
-    const copyTextarea = $('.copy-text');
-    copyTextarea.focus();
-    copyTextarea.select();
-    let successful = false;
-    try {
-      successful = document.execCommand('copy');
-    } catch (err) {
-      console.log('Unable to copy');
-    }
-    if (successful) {
-      const title = 'Copied successfully!';
-      $('.copy-tooltip').attr('title', title)
-          .tooltip('dispose')
-          .tooltip({'title': title})
-          .tooltip('show');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.copy-tooltip').forEach((tooltip) => {
+    const prompt = tooltip.dataset.bsTitle;
+
+    tooltip.addEventListener('click', () => {
+      bootstrap.Tooltip.getInstance(tooltip).dispose();
+      navigator.clipboard.writeText(tooltip.dataset.content).then(() => {
+        tooltip.dataset.bsTitle = tooltip.dataset.success;
+      }).catch(() => {
+        tooltip.dataset.bsTitle = tooltip.dataset.error;
+      }).finally(() => {
+        new bootstrap.Tooltip(tooltip).show();
+      });
+    });
+
+    tooltip.addEventListener('hidden.bs.tooltip', () => {
+      bootstrap.Tooltip.getInstance(tooltip).dispose();
+      tooltip.dataset.bsTitle = prompt;
+      new bootstrap.Tooltip(tooltip);
+    });
   });
 });
