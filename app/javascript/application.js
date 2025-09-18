d1d4e93b9c93f8ec0225d267dea48886a077f5dc
benmelz <ben@melz.me>
2025-09-18 16:17:59 -0400

    set up rails ujs correctly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
-import '@rails/ujs';
+import Rails from '@rails/ujs';
 import {Tooltip} from 'bootstrap';
 import './calendar.js';
 import './copyTooltip.js';
+
+Rails.start();
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((tooltipTriggerEl) => {
