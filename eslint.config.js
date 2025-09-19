792d83741385a0afbdc56373605386ac103e037a
benmelz <ben@melz.me>
2025-09-19 09:52:15 -0400

    convert config

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+const js = require('@eslint/js');
+const googleConfig = require('eslint-config-google');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: [
+      'app/assets/builds/*',
+      'coverage/*',
+      'node_modules/*',
+      'public/assets/*',
+    ],
+  },
+  {
+    files: ['**/*.js'],
+    ...js.configs.recommended,
+    ...googleConfig,
+    rules: {
+      ...js.configs.recommended.rules,
+      ...googleConfig.rules,
+      'max-len': ['error', {code: 120}],
+    },
+  },
+  {
+    files: ['app/assets/javascripts/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        'bootstrap': 'readonly',
+        'FullCalendar': 'readonly',
+      },
+    },
+  },
+  {
+    files: ['contrib/am-eve-split/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        'Runtime': 'readonly',
+        'Twilio': 'readonly',
+      },
+    },
+  },
+  {
+    files: ['*.config.js'],
+    languageOptions: {
+      globals: {...globals.node},
+    },
+  },
+];
