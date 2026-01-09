442473f176de827f397a81bde215a526667a65d1
benmelz <ben@melz.me>
2026-01-09 11:51:12 -0500

    scaffold out remaining todos

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,9 +1,17 @@
 import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['source'];
+  static targets = ['source', 'indicator'];
 
   copy() {
-    navigator.clipboard.writeText(this.sourceTarget.innerText);
+    navigator.clipboard.writeText(this.sourceTarget.innerText).then(() => {
+      // TODO: Change indicator to checkmark.
+    }).catch(() => {
+      // TODO: Change indicator to x mark.
+    });
+  }
+
+  reset() {
+    // TODO: Change indicator to clipboard.
   }
 }
