b1853f2e06eca3ea6ac63f99d20689f24962d01d
benmelz <ben@melz.me>
2026-01-09 13:54:49 -0500

    tweaks

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,34 +2,20 @@ import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
   static targets = ['source', 'indicator'];
-  static values = ['resetable'];
 
   copy() {
     navigator.clipboard.writeText(this.sourceTarget.innerText).then(() => {
-      this.indicatorTarget.animate({opacity: [100, 0]}, 150).finished.then(() => {
-        this.indicatorTarget.setAttribute('class', 'fa-solid fa-check no-pointer-events');
-        this.indicatorTarget.animate({opacity: [0, 100]}, 150);
-        this.resetableValue = true;
-      });
+      this.indicatorTarget.classList.remove('fa-clipboard');
+      this.indicatorTarget.classList.add('fa-check');
     }).catch(() => {
-      this.indicatorTarget.animate({opacity: [100, 0]}, 150).finished.then(() => {
-        this.indicatorTarget.setAttribute('class', 'fa-solid fa-xmark no-pointer-events');
-        this.indicatorTarget.animate({opacity: [0, 100]}, 150);
-        this.resetableValue = true;
-      });
+      this.indicatorTarget.classList.remove('fa-clipboard');
+      this.indicatorTarget.classList.add('fa-xmark');
     });
-  }
-  connect() {
-    console.log('connected');
   }
 
   reset() {
-    if (this.resetableValue) {
-      this.indicatorTarget.animate({opacity: [100, 0]}, 250).finished.then(() => {
-        this.indicatorTarget.setAttribute('class', 'fa-regular fa-clipboard no-pointer-events');
-        this.indicatorTarget.animate({opacity: [0, 100]}, 150);
-        this.resetableValue = false;
-      });
-    }
+    this.indicatorTarget.classList.remove('fa-check');
+    this.indicatorTarget.classList.remove('fa-xmark');
+    this.indicatorTarget.classList.add('fa-clipboard');
   }
 }
