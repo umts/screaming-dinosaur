a6ff9db243290bc853f2fc49ba8821592ac737a1
LateNightFlowerField <92184208+LateNightFlowerField@users.noreply.github.com>
2026-01-09 13:25:07 -0500

    add clipboard symbols and animations

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,16 +2,34 @@ import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
   static targets = ['source', 'indicator'];
+  static values = ['resetable'];
 
   copy() {
     navigator.clipboard.writeText(this.sourceTarget.innerText).then(() => {
-      // TODO: Change indicator to checkmark.
+      this.indicatorTarget.animate({opacity: [100, 0]}, 150).finished.then(() => {
+        this.indicatorTarget.setAttribute('class', 'fa-solid fa-check no-pointer-events');
+        this.indicatorTarget.animate({opacity: [0, 100]}, 150);
+        this.resetableValue = true;
+      });
     }).catch(() => {
-      // TODO: Change indicator to x mark.
+      this.indicatorTarget.animate({opacity: [100, 0]}, 150).finished.then(() => {
+        this.indicatorTarget.setAttribute('class', 'fa-solid fa-xmark no-pointer-events');
+        this.indicatorTarget.animate({opacity: [0, 100]}, 150);
+        this.resetableValue = true;
+      });
     });
+  }
+  connect() {
+    console.log('connected');
   }
 
   reset() {
-    // TODO: Change indicator to clipboard.
+    if (this.resetableValue) {
+      this.indicatorTarget.animate({opacity: [100, 0]}, 250).finished.then(() => {
+        this.indicatorTarget.setAttribute('class', 'fa-regular fa-clipboard no-pointer-events');
+        this.indicatorTarget.animate({opacity: [0, 100]}, 150);
+        this.resetableValue = false;
+      });
+    }
   }
 }
