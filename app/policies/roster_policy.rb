b8b81e75649feaf5f1939758e5ab2eef7321d26c
benmelz <ben@melz.me>
2026-01-13 09:50:36 -0500

    Implement roster policies (#771)

--- app/policies/roster_policy.rb
+++ app/policies/roster_policy.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class RosterPolicy < ApplicationPolicy
+  def index? = user&.admin?
+
+  def show? = user.present? || valid_api_key?
+
+  def new? = user&.admin?
+  alias create? new?
+
+  def edit? = user&.admin_in?(record)
+  alias update? edit?
+
+  def destroy? = user&.admin_in?(record)
+
   def assignments? = user.present?
+
+  def setup? = user&.admin_in?(record)
 end
