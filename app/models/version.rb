7e6ec2db8d4742c2b1586381c85437327f54528d
benmelz <ben@melz.me>
2026-01-13 13:50:18 -0500

    add author association

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,4 +2,6 @@
 
 class Version < ApplicationRecord
   include PaperTrail::VersionConcern
+
+  belongs_to :author, optional: true, class_name: 'User', foreign_key: :whodunnit, inverse_of: :authored_versions
 end
