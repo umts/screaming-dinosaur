4deabf52cfb30b503c26b5b4e4af2f662939426f
benmelz <ben@melz.me>
2026-01-13 15:06:02 -0500

    Implement assignment policy (#778)

--- app/policies/assignment_policy.rb
+++ app/policies/assignment_policy.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
 class AssignmentPolicy < ApplicationPolicy
+  authorize :roster, optional: true
+
   def index? = user.present?
+
+  def new? = user.present?
+
+  def create? = user.present? && (user&.admin_in?(record.roster) || not_assigning_someone_else?)
+
+  def edit? = user.present?
+
+  def update? = user.present? && (user&.admin_in?(record.roster) || not_assigning_someone_else?)
+
+  def destroy? = user&.admin_in?(record.roster)
+
+  def feed? = user&.member_of?(roster)
+
+  private
+
+  def not_assigning_someone_else?
+    return true if user&.admin_in?(record.roster)
+    return true unless record.user_changed?
+
+    record.user == user
+  end
 end
