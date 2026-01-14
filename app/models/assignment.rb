1c56733b329fce070115aa8fa3b29c333ecae4d0
benmelz <ben@melz.me>
2026-01-14 14:10:06 -0500

    use arel nodes to calculate times first

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -11,14 +11,7 @@ class Assignment < ApplicationRecord
   validate :user_in_roster
 
   scope :future, -> { where 'start_date > ?', Time.zone.today }
-  scope :at, lambda { |time|
-    where(start_date: nil..(time - 1.day))
-      .where(end_date: time..nil)
-      .or(where(start_date: time.at_midnight)
-            .where(rosters: { switchover: ...minutes_since_midnight }))
-      .or(where(end_date: time.at_midnight - 1.day)
-            .where(rosters: { switchover: minutes_since_midnight... })).joins(:roster)
-  }
+  scope :at, ->(time) { joins(:roster).where(start_time_node.lteq(time)).where(end_time_node.gt(time)) }
 
   def effective_start_datetime
     start_date + roster.switchover.minutes
@@ -60,6 +53,15 @@ class Assignment < ApplicationRecord
       Arel::Nodes::NamedFunction.new('IF', [minutes_since_midnight.gteq(switchover), today, yesterday])
     end
 
+    def start_time_node = time_node(:start_date)
+
+    def end_time_node
+      Arel::Nodes::NamedFunction.new 'TIMESTAMPADD',
+                                     [Arel::Nodes::SqlLiteral.new('DAY'),
+                                      Arel::Nodes::SqlLiteral.new('1'),
+                                      time_node(:end_date)]
+    end
+
     def in(roster)
       where roster:
     end
@@ -77,6 +79,15 @@ class Assignment < ApplicationRecord
       where(start_date: Date.tomorrow).find_each do |assignment|
         AssignmentsMailer.upcoming_reminder(assignment).deliver_now
       end
+    end
+
+    private
+
+    def time_node(column)
+      Arel::Nodes::NamedFunction.new 'TIMESTAMPADD',
+                                     [Arel::Nodes::SqlLiteral.new('MINUTE'),
+                                      Roster.arel_table[:switchover],
+                                      arel_table[column]]
     end
   end
 
