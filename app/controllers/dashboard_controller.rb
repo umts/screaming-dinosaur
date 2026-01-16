c9d2a8d44cb22db3b83e6751fd756e356993de4e
LateNightFlowerField <92184208+LateNightFlowerField@users.noreply.github.com>
2026-01-16 11:04:42 -0500

    update routing swap out roster index page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,10 @@
 class DashboardController < ApplicationController
   def index
     authorize!
-    return unless Current.user.rosters.one?
-
-    redirect_to roster_assignments_path(Current.user.rosters.first)
+    if Current.user.rosters.one?
+      redirect_to roster_assignments_path
+    else
+      redirect_to rosters_path
+    end
   end
 end
