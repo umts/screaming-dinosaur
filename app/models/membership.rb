f16a6d392a2692c33c42cebcc0fab66c138a9265
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-01-21 10:52:04 -0500

    Code cleanup and formatting

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -6,7 +6,7 @@ class Membership < ApplicationRecord
   belongs_to :roster
   validates :user, uniqueness: { scope: :roster }
   validate :at_least_one_admin
-  after_destroy :delete_future_assignment
+  after_destroy :delete_future_assignments
 
   private
 
@@ -16,10 +16,7 @@ class Membership < ApplicationRecord
     errors.add :user, 'is the last admin and cannot be demoted'
   end
 
-  def delete_future_assignment
-    user.assignments
-        .where(roster: roster)
-        .upcoming
-        .destroy_all
+  def delete_future_assignments
+    user.assignments.where(roster:).future.destroy_all
   end
 end
