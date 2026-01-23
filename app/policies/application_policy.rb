030163ff0352a1f3ed89289f3c2b3aa118ec2a1e
benmelz <ben@melz.me>
2026-01-23 12:27:20 -0500

    policy revamp

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -4,6 +4,10 @@ class ApplicationPolicy < ActionPolicy::Base
   authorize :user, allow_nil: true
   authorize :api_key, optional: true
 
+  alias_rule :index?, :create?, to: :manage?
+  alias_rule :new?, to: :create?
+  alias_rule :edit?, to: :update?
+
   protected
 
   def valid_api_key? = api_key.present? && api_key == Rails.application.credentials.fetch(:api_key)
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,8 +2,11 @@
 
 class UserPolicy < ApplicationPolicy
   def manage? = user&.admin
-  alias_rule :index?, :new?, :create?, to: :manage?
 
-  def edit? = manage? || (user == record)
-  alias_rule :update?, to: :edit?
+  def update?
+    return false unless manage? || record == user
+    return false if record.changes.slice('spire', 'admin', 'active').present? && !manage?
+
+    true
+  end
 end
