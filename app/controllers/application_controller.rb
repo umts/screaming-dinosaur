b4dc1c15a1a34b8a86689498ffff995b94d83ddf
benmelz <ben@melz.me>
2026-01-29 10:33:16 -0500

    flash infrastructure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def success_message_for(subject, action = nil)
-    subject = subject.model_name.element if subject.respond_to?(:model_name)
-    action ||= action_name
-    t('success.message', action: t("success.actions.#{action}"), subject:)
-  end
+  def flash_success_for(subject, action = nil, undoable: false) = flash_success(flash, subject, action, undoable)
+
+  def flash_errors_for(subject) = flash_errors(flash, subject)
+
+  def flash_errors_now_for(subject) = flash_errors(flash.now, subject)
 
   def confirm_change(object, message = nil)
     # Rubocop can't tell whether we're redirecting after this or not.
@@ -30,6 +30,21 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def flash_success(flash, subject, action, undoable)
+    flash[:notice] = success_message_for(subject, action)
+    flash[:undo] = subject.versions.last.id if undoable
+  end
+
+  def flash_errors(flash, subject)
+    flash[:alert] = subject.errors.full_messages.to_sentence
+  end
+
+  def success_message_for(subject, action)
+    subject = subject.model_name.element if subject.respond_to?(:model_name)
+    action ||= action_name
+    t('success.message', action: t("success.actions.#{action}"), subject:)
+  end
 
   def set_roster
     @roster = Roster.friendly.find(params[:roster_id], allow_nil: true) || Current.user&.rosters&.first || Roster.first
