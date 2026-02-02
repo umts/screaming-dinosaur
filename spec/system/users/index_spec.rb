1f519fa5471629239644dbd3629d86f50ddc7708
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-02-02 13:16:12 -0500

    fix remove active from users

--- spec/system/users/index_spec.rb
+++ spec/system/users/index_spec.rb
@@ -1,28 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe 'user index' do
-  describe 'deactivating a user', :js do
-    before do
-      roster = create :roster
-      create :user, first_name: 'Bobo', last_name: 'Test', memberships: [build(:membership, roster:)]
-
-      current_user = create :user, memberships: [build(:membership, roster:, admin: true)]
-      login_as current_user
-
-      visit root_path
-      click_on 'Manage Users'
-      within('tr', text: 'Bobo Test') { accept_alert { click_button 'Deactivate' } }
-    end
-
-    it 'no longer lists the user on the roster index' do
-      expect(page).to have_no_text('Bobo Test')
-    end
-
-    it 'informs you of success' do
-      expect(page).to have_text('Successfully updated user')
-    end
-  end
-
   context 'when viewing the index' do
     let(:roster) { create :roster }
     let(:admin_membership) { create :membership, roster:, admin: true }
