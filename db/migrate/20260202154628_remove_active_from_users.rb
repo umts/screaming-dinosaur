9910d397b3b57dbd1440615ca6867db9db620f1b
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-02-02 12:43:43 -0500

    Remove active column from users table

--- db/migrate/20260202154628_remove_active_from_users.rb
+++ db/migrate/20260202154628_remove_active_from_users.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class RemoveActiveFromUsers < ActiveRecord::Migration[8.1]
-  def change; end
+  def change
+   remove_column :users, :active, :boolean, default: true
+  end
 end
