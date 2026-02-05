5ddbf435a15fcbd717c379cde1cedab8885211b1
benmelz <ben@melz.me>
2026-02-05 11:50:23 -0500

    user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 class UserPolicy < ApplicationPolicy
-  def manage? = admin?
+  def manage? = false
 
-  def update?
-    return false unless manage? || record == user
-    return false if record.changes.slice('spire', 'admin', 'active').present? && !manage?
-
-    true
-  end
+  def update? = record == user && record.changes.slice('spire', 'admin', 'active').blank?
 end
