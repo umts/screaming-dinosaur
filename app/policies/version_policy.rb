ba7516dd3bd585710b557873eb70f320f1ab99a9
benmelz <ben@melz.me>
2026-02-05 11:48:35 -0500

    easy policies

--- app/policies/version_policy.rb
+++ app/policies/version_policy.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class VersionPolicy < ApplicationPolicy
-  def undo? = user.present? && user == record.author
+  def undo? = logged_in? && user == record.author
 end
