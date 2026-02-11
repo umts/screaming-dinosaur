06d16fa95d66b7a81d699e85dd3c560a584f2ffa
benmelz <ben@melz.me>
2026-02-11 10:08:04 -0500

    turn tooltip into popover

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 import Rails from '@rails/ujs';
-import {Tooltip} from 'bootstrap';
+import {Popover} from 'bootstrap';
 import './controllers/index.js';
 
 Rails.start();
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((tooltipTriggerEl) => {
-    new Tooltip(tooltipTriggerEl);
+  document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popoverTriggerEl) => {
+    new Popover(popoverTriggerEl);
   });
 });
