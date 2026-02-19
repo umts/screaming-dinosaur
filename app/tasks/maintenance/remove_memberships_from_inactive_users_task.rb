e955d4cd9c2ffaedb63d5356c8be3d873699f915
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-02-19 12:20:48 -0500

    remove comment lines

--- app/tasks/maintenance/remove_memberships_from_inactive_users_task.rb
+++ app/tasks/maintenance/remove_memberships_from_inactive_users_task.rb
@@ -4,15 +4,10 @@ module Maintenance
   class RemoveMembershipsFromInactiveUsersTask < MaintenanceTasks::Task
     def collection
       User.inactive.joins(:memberships)
-      # Collection to be iterated over
-      # Must be Active Record Relation or Array
     end
 
     def process(user)
       user.memberships.destroy_all
-      # The work to be done in a single iteration of the task.
-      # This should be idempotent, as the same element may be processed more
-      # than once if the task is interrupted and resumed.
     end
   end
 end
