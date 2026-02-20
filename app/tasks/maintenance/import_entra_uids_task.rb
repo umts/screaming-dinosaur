85c156a674de393f293f037ac8779fac6b7262db
benmelz <ben@melz.me>
2026-02-20 10:33:29 -0500

    add entra uids task

---
+++ app/tasks/maintenance/import_entra_uids_task.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class ImportEntraUidsTask < MaintenanceTasks::Task
+    csv_collection
+
+    def process(row)
+      User.find_by!(spire: "#{row[:spire_id]}@umass.edu").update!(entra_uid: row[:entra_uid])
+    end
+  end
+end
