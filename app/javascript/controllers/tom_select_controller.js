e1ed6b3b4b3b00e472deccbf52d2af2c62b838ad
benmelz <ben@melz.me>
2026-02-25 12:17:54 -0500

    basic configuration

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -3,6 +3,9 @@ import TomSelect from 'tom-select';
 
 export default class extends Controller {
   connect() {
-    new TomSelect(this.element, {});
+    new TomSelect(this.element, {
+      create: false,
+      plugins: ['dropdown_input'],
+    });
   }
 }
