e41402ec0197d3424cd0e88bdd707fe118f8c6d3
benmelz <ben@melz.me>
2026-02-26 15:39:54 -0500

    Drop old auth columns

---
+++ db/migrate/20260226203905_drop_old_auth_columns.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class DropOldAuthColumns < ActiveRecord::Migration[8.1]
+  def change
+    remove_column :users, :spire, :string
+    remove_column :users, :shibboleth_eppn, :string
+  end
+end
