52656219c5a027914dc27f1a87ed54978639883d
benmelz <ben@melz.me>
2026-03-06 13:18:02 -0500

    move schedule from whenever to solid queue

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
 env 'PATH', '/opt/ruby/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-
-every :day, at: '9:00am' do
-  runner 'SendAssignmentRemindersJob.perform_now'
-end
-
-every :day, at: '4:00am' do
-  runner 'CheckRostersUncoveredDatesJob.perform_now'
-end
