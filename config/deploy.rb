33255a06a101e6c8280e4975fbe1049abf0a482e
benmelz <ben@melz.me>
2026-03-09 09:34:52 -0400

    Drop whenever (#902)

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,6 @@ set :deploy_to, "/srv/#{fetch :application}"
 
 set :log_level, :info
 
-set :whenever_command, %i[sudo bundle exec whenever]
-
 append :linked_files,
        'config/database.yml',
        'config/credentials/production.key'
