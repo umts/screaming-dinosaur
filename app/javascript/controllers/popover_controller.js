99aaec4adc0d2e35175dd6d7ab527aeed03fecb8
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-03-13 01:29:30 -0400

    reimplement code as stimulus controller

---
+++ app/javascript/controllers/popover_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import { Popover } from "bootstrap"
+// Connects to data-controller="popover"
+export default class extends Controller {
+  connect() {
+    new Popover(this.element)
+  }
+}
