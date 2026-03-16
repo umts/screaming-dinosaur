75f4b60283c2d1f3469cdae3fe77e4a10d75e318
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-03-16 12:01:53 -0400

    Convert popover to use stimulus controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,5 @@
 import Rails from '@rails/ujs';
-import {Popover} from 'bootstrap';
 import './controllers/index.js';
 
 Rails.start();
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popoverTriggerEl) => {
-    new Popover(popoverTriggerEl);
-  });
-});
