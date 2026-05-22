04b80c1d8d8bdec641691153e048278f0a258704
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-05-22 09:15:55 -0400

    fix req/assignments specs

--- app/policies/assignment_policy.rb
+++ app/policies/assignment_policy.rb
@@ -3,7 +3,11 @@
 class AssignmentPolicy < ApplicationPolicy
   authorize :roster, optional: true
 
-  def manage? = allowed_to?(:manage?, record.roster)
+  def manage? = allowed_to?(:manage?, record.roster) || (allowed_to?(:show?, record.roster) && record.user_id == user.id)
 
   def index? = allowed_to?(:show?, roster)
+
+  def new? =  allowed_to?(:show?, record.roster) 
+
+  def edit? =  allowed_to?(:show?, record.roster) 
 end
