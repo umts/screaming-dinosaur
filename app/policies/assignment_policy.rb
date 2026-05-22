3d0b5d048999f89954027b4f7bf11d1dbc8207e5
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-05-22 09:20:18 -0400

    fix rubocop offenses

--- app/policies/assignment_policy.rb
+++ app/policies/assignment_policy.rb
@@ -3,11 +3,14 @@
 class AssignmentPolicy < ApplicationPolicy
   authorize :roster, optional: true
 
-  def manage? = allowed_to?(:manage?, record.roster) || (allowed_to?(:show?, record.roster) && record.user_id == user.id)
+  def manage?
+    allowed_to?(:manage?,
+                record.roster) || (allowed_to?(:show?, record.roster) && record.user_id == user.id)
+  end
 
   def index? = allowed_to?(:show?, roster)
 
-  def new? =  allowed_to?(:show?, record.roster) 
+  def new? = allowed_to?(:show?, record.roster)
 
-  def edit? =  allowed_to?(:show?, record.roster) 
+  def edit? = allowed_to?(:show?, record.roster)
 end
