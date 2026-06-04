0d9306cc44f338dcca5221b3219247d7becc0397
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-06-04 07:35:03 -0400

    change to datetime for roster uncovered dates job

--- app/jobs/check_rosters_uncovered_dates_job.rb
+++ app/jobs/check_rosters_uncovered_dates_job.rb
@@ -3,7 +3,7 @@
 class CheckRostersUncoveredDatesJob < ApplicationJob
   def perform
     Roster.find_each do |roster|
-      open_dates = roster.uncovered_dates_between(Time.zone.now, 2.weeks.from_now)
+      open_dates = roster.uncovered_datetimes_between(Time.current, 2.weeks.from_now)
       next if open_dates.empty? || roster.admins.empty?
 
       RosterMailer.with(roster:, open_dates:).open_dates_alert.deliver_now
