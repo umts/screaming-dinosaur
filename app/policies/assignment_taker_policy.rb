bfa0cb47b03862122f9783b47c663321c880a838
KernSharma <kern.sharma@gmail.com>
2026-06-17 14:44:08 -0700

    Restrict assignment taking to roster members

--- app/policies/assignment_taker_policy.rb
+++ app/policies/assignment_taker_policy.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class AssignmentTakerPolicy < ApplicationPolicy
-  def manage? = user.present?
+  def manage? = record.assignment.present? && allowed_to?(:show?, record.assignment.roster)
 end
--- spec/requests/assignment_takers_spec.rb
+++ spec/requests/assignment_takers_spec.rb
@@ -3,18 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Assignment Takers' do
-  let(:current_user) { create :user }
   let(:roster) { create :roster }
+  let(:current_user) { create :user, memberships: [build(:membership, roster:, admin: false)] }
 
   describe 'GET /assignments/:id/take' do
     subject(:call) { get "/assignments/#{assignment.id}/take" }
 
     let(:assignment) { create :assignment, roster:, user: nil }
 
-    context 'when logged in as a user' do
+    context 'when logged in as a member of the roster' do
       it 'responds successfully' do
         call
         expect(response).to be_successful
+      end
+    end
+
+    context 'when logged in as a user unrelated to the roster' do
+      let(:current_user) { create :user }
+
+      it 'responds with a forbidden status' do
+        call
+        expect(response).to have_http_status(:forbidden)
       end
     end
 
@@ -35,6 +44,20 @@ RSpec.describe 'Assignment Takers' do
     subject(:submit) { post "/assignments/#{assignment.id}/take", params: { assignment_taker: attributes } }
 
     let(:attributes) { {} }
+
+    context 'when logged in as a user unrelated to the roster' do
+      let(:assignment) { create :assignment, roster:, user: nil }
+      let(:current_user) { create :user }
+
+      it 'responds with a forbidden status' do
+        submit
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'does not assign the user' do
+        expect { submit }.not_to(change { assignment.reload.user })
+      end
+    end
 
     context 'with an ungrouped assignment' do
       let(:assignment) { create :assignment, roster:, user: nil }
