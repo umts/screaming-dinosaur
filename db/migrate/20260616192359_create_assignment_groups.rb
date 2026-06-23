67e854f500a6f7e2e3acfc1eb0e092fea2bf612c
benmelz <ben@melz.me>
2026-06-23 11:40:06 -0400

    flatten migrations

--- db/migrate/20260616192359_create_assignment_groups.rb
+++ db/migrate/20260616192359_create_assignment_groups.rb
@@ -2,7 +2,10 @@
 
 class CreateAssignmentGroups < ActiveRecord::Migration[8.1]
   def change
-    create_table :assignment_groups, &:timestamps
+    create_table :assignment_groups do |t|
+      t.string :name, null: false
+      t.timestamps
+    end
 
     add_reference :assignments, :assignment_group, null: true, foreign_key: true
   end
--- db/migrate/20260617205830_add_name_to_assignment_groups.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddNameToAssignmentGroups < ActiveRecord::Migration[8.1]
-  def change
-    add_column :assignment_groups, :name, :string, null: false
-  end
-end
