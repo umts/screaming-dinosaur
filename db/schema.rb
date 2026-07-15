9e305e69e157162fa7f0e2f883fbf71ddb105d05
benmelz <ben@melz.me>
2026-07-15 11:36:52 -0400

    fold assignment group migration in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_16_192359) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_12_172421) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_520_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
