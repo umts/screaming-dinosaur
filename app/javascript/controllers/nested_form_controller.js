e73785b00cd1817cc19f4c1b7422dbc9f96d5a2d
Zafnun Hasnat <hasnatzafnun@gmail.com>
2026-07-28 11:28:31 -0400

    Fix linting

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,17 +1,17 @@
 
-import { Controller } from '@hotwired/stimulus'
+import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['template', 'list']
+  static targets = ['template', 'list'];
 
   add() {
-    const uniqueId = new Date().getTime()
-    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, uniqueId)
+    const uniqueId = new Date().getTime();
+    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, uniqueId);
 
-    this.listTarget.insertAdjacentHTML('beforeend', content)
+    this.listTarget.insertAdjacentHTML('beforeend', content);
   }
 
   remove(event) {
-    event.target.closest('[data-nested-form-target="item"]').remove()
+    event.target.closest('[data-nested-form-target="item"]').remove();
   }
 }
